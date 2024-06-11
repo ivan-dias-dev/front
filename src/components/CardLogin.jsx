bf3960a5933ed2fa2f8171d44ac1777b76f1abc3
ivan-dias-dev <mangakaivan@gmail.com>
2024-06-11 11:08:49 -0300

    att css

--- src/components/CardLogin.jsx
+++ src/components/CardLogin.jsx
@@ -6,18 +6,18 @@ function CardLogin() {
                 <div className="d-flex justify-content-center">
                     <div className={Styles.InputLogin}>
                         <div className={Styles.centroCusto}>Centro de custo</div>
-                        <input type="text" required/>
+                        <input type="text" required />
                     </div>
                 </div>
                 <div className={Styles.PaiBlocotNomeCPF}>
                     <div className={Styles.BlocotNomeCPF}>
                         <div className={Styles.InputLogin2}>
                             <div className={Styles.nome} >Nome</div>
-                            <textarea type="text" required/>
+                            <textarea type="text" required />
                         </div>
                         <div className={Styles.InputLogin2}>
                             <div className={Styles.cpf}>CPF</div>
-                            <textarea type="text" required/>
+                            <textarea type="text" required />
                         </div>
                     </div>
                 </div>
